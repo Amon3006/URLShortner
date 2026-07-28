86b222aff3ebb1ef20a36df6379fb9b85b147fc9
Aman <aman.raibole.30072003@gmail.com>
2026-07-29 00:43:48 +0530

    swagger fixes

--- backend/UrlShortener/UrlShortener.Api/Program.cs
+++ backend/UrlShortener/UrlShortener.Api/Program.cs
@@ -50,11 +50,9 @@ namespace UrlShortener.Api
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
